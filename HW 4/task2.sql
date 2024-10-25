272327ab964d76a400e44ba4369cd6d3e4cef580
Lamin-Ceesay <lamin.ceesay.2@city.ac.uk>
2024-10-25 09:52:13 +0100

    Update task2.sql

--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -1,10 +1,14 @@
+ON DELETE
+FOREIGN KEY petname REFERENCES petPet(petname)
+ON DELETE CASCADE; 
+
 UPDATE pet_database.petevent t
 SET t.remark = '5 kittens, 3 female, 2 male'
-WHERE t.petname LIKE 'Fluffy' ESCAPE '#' AND t.eventdate = 950515;
+WHERE t.petname LIKE 'Fluffy' AND t.eventdate = 950515;
 
 UPDATE pet_database.petevent t
 SET t.petname = 'Claws'
-WHERE t.petname LIKE 'Slim' ESCAPE '#' AND t.eventdate = 970803;
+WHERE t.petname LIKE 'Slim' AND t.eventdate = 970803;
 
 INSERT INTO pet_database.petevent (petname, eventdate, eventtype, remark)
 VALUES ('Fluffy', 201015, 'vet', 'antibiotics');
@@ -14,20 +18,20 @@ VALUES ('Hammy', 201015, 'vet', 'antibiotics');
 
 DELETE
 FROM pet_database.petevent
-WHERE petname LIKE 'Buffy' ESCAPE '#' AND eventdate = 930623;
+WHERE petname LIKE 'Buffy' AND eventdate = 930623;
 
 DELETE
 FROM pet_database.petevent
-WHERE petname LIKE 'Buffy' ESCAPE '#' AND eventdate = 940619;
+WHERE petname LIKE 'Buffy' AND eventdate = 940619;
 
 UPDATE pet_database.petpet t
 SET t.death = 200901
-WHERE t.petname LIKE 'Puffball' ESCAPE '#';
+WHERE t.petname LIKE 'Puffball';
 
 INSERT INTO pet_database.petpet (petname, owner, species, gender, birth, death)
 VALUES ('Hammy', 'Diane', 'hamster', 'M', 101030, null);
 
 DELETE
 FROM pet_database.petpet
-WHERE petname LIKE 'Buffy' ESCAPE '#';
+WHERE petname LIKE 'Buffy';
 
